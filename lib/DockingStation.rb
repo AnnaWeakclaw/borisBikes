919e4637cd36ef29dee576b68e96bd934a7ea752
Anna <annas.wieclaw@gmail.com>
2019-05-13 17:10:59 +0100

    Can return and dock the bike and see the bike that has been docked

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,7 +1,17 @@
 require_relative 'Bike'
 class DockingStation
+  attr_reader :bike
+
+  def initialize(bike)
+    @bike = bike
+  end
 
   def release_bike
     return Bike.new
+  end
+
+  def dock 
+   bike
+
   end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,19 +3,38 @@ require "DockingStation"
 describe DockingStation do 
   describe "releases a bike" do
   it do 
-    # docking_station = DockingStation.new
-    # expect(docking_station).to respond_to(:release_bike)
-    is_expected.to respond_to(:release_bike)
+    bike = Bike.new
+    docking_station = DockingStation.new(bike)
+    expect(docking_station).to respond_to(:release_bike)
+    #is_expected.to respond_to(:release_bike)
   end
 end
 
   describe "gets a bike" do
     it { 
-
-      docking_station = DockingStation.new
+      bike = Bike.new
+      docking_station = DockingStation.new(bike)
       bike = docking_station.release_bike
 
       expect(bike.working?).to eq(true)
            }
+  end
+
+  describe "docks a bike" do
+    it {
+      bike = Bike.new
+      docking_station = DockingStation.new(bike)
+
+      expect(docking_station).to respond_to(:dock)
+    }
+  end
+
+  describe "see a bike that has been docked" do
+    it {
+      bike = Bike.new
+      docking_station = DockingStation.new(bike)
+
+      expect(docking_station.dock).to eq(bike)
+    }
   end
 end 
